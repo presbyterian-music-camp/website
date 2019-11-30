245fbe25bde923996e264f2b4fdd986d9b7357ad
presbyterian-music-camp <58372879+presbyterian-music-camp@users.noreply.github.com>
2019-11-30 16:45:42 -0500

    Updated from Publii

--- assets/js/svg-fix.js
+++ assets/js/svg-fix.js
@@ -1,19 +1,17 @@
-// SVG map fix
-(function() {
-    var allItems = document.querySelectorAll('use');
+(function($) {
+    var allItems = $('use');
 
-    for (var i = 0; i < allItems.length; i++) {
-        var item = allItems[i];
-        var anchor = '#' + item.getAttribute('xlink:href').split('#')[1];
+    allItems.each(function(i, item) {
+        item = $(item);
+        var anchor = '#' + item.attr('xlink:href').split('#')[1];
         var itemData = window.publiiSvgFix[anchor];
 
         if(!itemData) {
-            console.log('ANCHOR', anchor, i);
-            continue;
+            return;
         }
 
-        var svgItem = item.parentNode;
-        svgItem.innerHTML = itemData.content;
-        svgItem.setAttribute('viewBox', itemData.viewbox);
-    }
-})();
+        var svgItem = item.parent();
+        svgItem.html(itemData.content);
+        svgItem.attr('viewBox', itemData.viewbox);
+    });
+})(jQuery);
